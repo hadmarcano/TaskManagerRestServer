d83eeecbfe2fc3d3aaea1552f54172c49e16cc09
Hector Adolfo <hadmarcano@gmail.com>
2021-02-01 23:59:20 -0300

    add populate to GET/tasks router

--- src/routes/task.js
+++ src/routes/task.js
@@ -15,10 +15,13 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
 router.get("/tasks", auth, async (req, res) => {
   try {
-    const tasks = await Task.find({ owner: req.user._id });
-    res.send(tasks);
+    // const tasks = await Task.find({ owner: req.user._id });
+    // res.send(tasks);
+    await req.user.populate("tasks").execPopulate();
+    req.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
   }
